d0eca11e46328874126e51d40e9eec3675784820
DAVIDCOQUE <david03sc@gmail.com>
2024-09-30 14:21:51 -0500

    create component personas, persona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { personasComponent } from './personas/personas.component';
+import { PersonaComponent } from './persona/persona.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    
+    AppComponent,personasComponent, PersonaComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
