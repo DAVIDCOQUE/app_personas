8a9dbf77713fbd3e06ad7fbfd55e107b6783f401
DAVIDCOQUE <david03sc@gmail.com>
2024-10-01 06:39:49 -0500

    new changes app-person

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { personasComponent } from './personas/personas.component';
-import { PersonaComponent } from './persona/persona.component';
 import { FormsModule } from '@angular/forms';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
-    
-    AppComponent,personasComponent, PersonaComponent
+
+    AppComponent,
+    PersonaComponent
   ],
   imports: [
     FormsModule,
